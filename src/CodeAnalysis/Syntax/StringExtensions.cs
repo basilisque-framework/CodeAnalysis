366b39bf2d22be088636c822461e5bbb94a9766b
Alexander Stärk <alexander.staerk@asprog.de>
2022-04-25 15:28:18 +0200

    #1 - added extension methods for converting a string to a valid namespace/identifier

---
+++ src/CodeAnalysis/Syntax/StringExtensions.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+
+#if BASILISQUE_BENCHMARK
+namespace Basilisque.CodeAnalysis.Syntax.Benchmark
+#else
+namespace Basilisque.CodeAnalysis.Syntax
+#endif
+{
+    /// <summary>
+    /// Extension methods for <see cref="string"/> regarding CodeAnalysis.Syntax
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Checks if the given string is a reserved keyword
+        /// </summary>
+        /// <param name="s">The string to check</param>
+        /// <returns>A bool stating if the given string is a keyword or not</returns>
+        public static bool IsReservedKeyword(this string? s)
+        {
+            return SyntaxFacts.IsReservedKeyword(SyntaxFacts.GetKeywordKind(s!));
+        }
+
+        /// <summary>
+        /// Converts a string to a valid C# namespace
+        /// </summary>
+        /// <param name="source">The string that should be converted</param>
+        /// <returns>A string containing a valid namespace or null</returns>
+        public static string? ToValidNamespace(this string? source)
+        {
+            var result = toValidIdentifier(source, c => c == '.' || SyntaxFacts.IsIdentifierPartCharacter(c));
+
+            if (result == null)
+                return result;
+
+            var parts = result.Split('.');
+
+            if (parts.Length > 1)
+            {
+                char[] resultArr = new char[result.Length + parts.Length];
+                var targetIdx = 0;
+
+                var lastIdx = parts.Length - 1;
+                for (int i = 0; i < parts.Length; i++)
+                {
+                    var part = parts[i];
+
+                    if (part.IsReservedKeyword())
+                    {
+                        resultArr[targetIdx] = '@';
+                        targetIdx++;
+                    }
+
+                    foreach (var c in part)
+                    {
+                        resultArr[targetIdx] = c;
+                        targetIdx++;
+                    }
+
+                    if (i < lastIdx)
+                    {
+                        resultArr[targetIdx] = '.';
+                        targetIdx++;
+                    }
+                }
+
+                return new string(resultArr, 0, targetIdx);
+            }
+            else
+                return result.IsReservedKeyword() ? "@" + result : result;
+        }
+
+        /// <summary>
+        /// Converts a string to a valid C# identifier
+        /// </summary>
+        /// <param name="source">The string that should be converted</param>
+        /// <returns>A string containing a valid identifier or null</returns>
+        public static string? ToValidIdentifier(this string? source)
+        {
+            var result = toValidIdentifier(source, SyntaxFacts.IsIdentifierPartCharacter);
+
+            return result.IsReservedKeyword() ? "@" + result : result;
+        }
+
+        private static string? toValidIdentifier(string? source, Func<char, bool> isIdentifierPartCharacter)
+        {
+            if (string.IsNullOrEmpty(source))
+                return null;
+
+            int sourceLength = source!.Length;
+
+            char[] resultArr;
+            int targetIdx;
+            var currentChar = source[0];
+            var isStartChar = SyntaxFacts.IsIdentifierStartCharacter(currentChar);
+            if (isStartChar || !isIdentifierPartCharacter(currentChar))
+            {
+                resultArr = new char[sourceLength];
+                targetIdx = 0;
+
+                if (!isStartChar)
+                    currentChar = '_';
+            }
+            else
+            {
+                resultArr = new char[sourceLength + 1];
+                resultArr[0] = '_';
+                targetIdx = 1;
+            }
+
+            resultArr[targetIdx] = currentChar;
+
+            for (int srcIdx = 1; srcIdx < sourceLength; srcIdx++)
+            {
+                targetIdx++;
+
+                currentChar = source[srcIdx];
+                if (isIdentifierPartCharacter(currentChar))
+                    resultArr[targetIdx] = currentChar;
+                else
+                    resultArr[targetIdx] = '_';
+            }
+
+            return new string(resultArr);
+        }
+
+    }
+}
